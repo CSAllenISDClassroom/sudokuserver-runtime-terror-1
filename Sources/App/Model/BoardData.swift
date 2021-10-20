763661c7ea0e354826baf33ab9e996f620facc9c
Caleb Lee <caleb.lee@student.allenisd.org>
2021-10-20 14:11:12 -0500

    Separate branch for updating the data struct of storing randomized board information created

---
+++ Sources/App/Model/BoardData.swift
@@ -0,0 +1,53 @@
+struct Position: Codable {
+    let boxIndex : Int
+    let cellIndex : Int
+    init(boxIndex:Int, cellIndex:Int) {
+        self.boxIndex = boxIndex
+        self.cellIndex = cellIndex
+    }
+
+    init(row:Int, col:Int) {
+        boxIndex = (row - 1) / 3 * 3 + (col + 1) % 3
+        cellIndex = (row + 1) % 3 + (col + 1) % 3
+        //        let row = (boxIndex - 1) / 3 * 3 + (cellIndex - 1) / 3 + 1
+        //        let col = (boxIndex - 1) % 3 * 3 + (cellIndex - 1) % 3 + 1
+
+    }
+}
+
+struct Cell: Codable {
+    let position: Position
+    var value: Int
+}
+
+struct Box: Codable {
+    var cells: [Cell]
+
+    init(cells: [Cell]) {
+        self.cells = cells
+    }
+
+    init(boxIndex: Int) {
+        var cells = [Cell]()
+        for cellIndex in 0 ..< 9 {
+            cells.append(Cell(position: Position(boxIndex: boxIndex, cellIndex: cellIndex), value: cellIndex))
+        }
+        self.cells = cells
+    }
+}
+
+struct BoardStruct: Codable {
+    var board: [Box]
+
+    init() {
+        var board = [Box]()
+        for boxIndex in 0 ..< 9 {
+            board.append(Box(boxIndex: boxIndex))
+        }
+        self.board = board
+    }
+
+    init(board: [Box]) {
+        self.board = board
+    }
+}
---
+++ Sources/App/Model/QueryParameters.swift
@@ -0,0 +1,12 @@
+enum Difficulty {
+    case easy
+    case medium
+    case hard
+    case hell
+}
+
+enum Filter {
+    case all
+    case repeated
+    case incorrect
+}
