0dbacff1a7415d419795009b8ed7d3cc53812eab
Phat Bui <phat.bui@student.allenisd.org>
2021-10-22 14:18:11 -0500

    Deleted enum Difficulty and Filter from Data.swift

--- Sources/VaporShell/App/Model/Data.swift
+++ Sources/VaporShell/App/Model/Data.swift
@@ -39,19 +39,6 @@ struct Board: Codable {
     }
 }
 
-enum Difficulty: String {
-    case easy
-    case medium
-    case hard
-    case hell
-}
-
-enum Filter: String {
-    case all
-    case repeated
-    case incorrect
-}
-
 /// JSON ENCODING ///
 
 struct BoardId: Codable {
